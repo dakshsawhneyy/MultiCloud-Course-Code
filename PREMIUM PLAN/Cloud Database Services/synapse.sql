0bbb1cccf046d100540e1de66532570476ead712
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-10 16:25:23 +0530

    Create synapse.sql

---
+++ PREMIUM PLAN/Cloud Database Services/synapse.sql
@@ -0,0 +1,18 @@
+-- sales.csv
+order_id,product,region,quantity,revenue
+1,Laptop,North,1,80000
+2,Mouse,North,2,3000
+3,Laptop,South,1,80000
+4,Keyboard,West,3,9000
+5,Monitor,North,2,40000
+
+
+-- Queries:
+
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://<storage-account>.dfs.core.windows.net/synapse-data/sales.csv',
+    FORMAT = 'CSV',
+    PARSER_VERSION = '2.0',
+    HEADER_ROW = TRUE
+) AS sales;
