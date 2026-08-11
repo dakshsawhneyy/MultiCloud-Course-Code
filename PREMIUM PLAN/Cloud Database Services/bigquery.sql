73346feb8640092a1b7ac9802f999ce0b8f4a51d
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-11 11:57:00 +0530

    Create bigquery.sql

---
+++ PREMIUM PLAN/Cloud Database Services/bigquery.sql
@@ -0,0 +1,9 @@
+-- sales.csv
+order_id,customer_id,product,region,quantity,revenue,order_date
+1,101,Laptop,North,1,80000,2026-01-10
+2,102,Mouse,North,2,3000,2026-01-12
+3,103,Laptop,South,1,80000,2026-02-05
+4,104,Keyboard,West,3,9000,2026-02-10
+5,105,Monitor,North,2,40000,2026-03-01
+6,106,Laptop,West,2,160000,2026-03-15
+
