9d32a5f394a58db97635cf1316eb90f9a1f8b9a9
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-19 15:31:43 +0530

    Update logs.sh

--- PREMIUM PLAN/Monitoring & Basic Observability/logs.sh
+++ PREMIUM PLAN/Monitoring & Basic Observability/logs.sh
@@ -13,3 +13,49 @@ INFO Student application started
 INFO Student 101 retrieved profile
 WARN Database connection pool is high
 ERROR Database connection timeout
+
+#################################
+# Send logs to cloudwatch loggroup
+
+sudo systemctl status amazon-cloudwatch-agent
+
+#Install the cloud-watch agent
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+sudo systemctl status amazon-cloudwatch-agent
+
+# Add forwarding log group
+sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
+sudo nano /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
+# Put this in it:
+{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/student-app/app.log",
+            "log_group_name": "/student-loggroup",
+            "log_stream_name": "{instance_id}",
+            "retention_in_days": 7
+          }
+        ]
+      }
+    }
+  }
+}
+
+
+# Verify
+sudo cat /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
+
+# Start the agent with your config:
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config \
+  -m ec2 \
+  -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+  -s
+
