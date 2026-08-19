151ef8c48f307b65a238a71558ab72675ce5fffc
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-19 11:02:47 +0530

    Create setup.sql

---
+++ PREMIUM PLAN/Cloud Security Essentials/student management/setup.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(150),
+    course VARCHAR(100)
+);
+
+INSERT INTO students (name, email, course)
+VALUES ('Rahul', 'rahul@example.com', 'Computer Science');
+
+SELECT * FROM students;
