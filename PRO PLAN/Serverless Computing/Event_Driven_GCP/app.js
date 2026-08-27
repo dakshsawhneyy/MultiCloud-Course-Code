8ce8c1847b8157d53bce687d5d1db5add36d1e56
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-27 16:07:45 +0530

    Create app.js

---
+++ PRO PLAN/Serverless Computing/Event_Driven_GCP/app.js
@@ -0,0 +1,31 @@
+const functions = require('@google-cloud/functions-framework');
+const { Firestore } = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+    databaseId: 'your-custom-database-name'
+});
+
+functions.cloudEvent('processUpload', async cloudEvent => {
+    const data = cloudEvent.data;
+
+    const bucket = data.bucket;
+    const objectName = data.name;
+    const size = Number(data.size || 0);
+
+    console.log(`Processing: gs://${bucket}/${objectName}`);
+
+    const documentId = `${bucket}/${objectName}`;
+
+    await firestore
+        .collection('fileMetadata')
+        .doc(documentId)
+        .set({
+            fileName: objectName.split('/').pop(),
+            bucket: bucket,
+            objectName: objectName,
+            size: size,
+            uploadedAt: new Date().toISOString()
+        });
+
+    console.log('Metadata stored successfully');
+});
