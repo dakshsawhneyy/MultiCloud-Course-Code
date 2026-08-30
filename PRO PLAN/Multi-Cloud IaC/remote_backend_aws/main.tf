80f299361727a16bdea064d2a1f2f5f6ee111d36
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-30 20:26:13 +0530

    Update main.tf

--- PRO PLAN/Multi-Cloud IaC/remote_backend_aws/main.tf
+++ PRO PLAN/Multi-Cloud IaC/remote_backend_aws/main.tf
@@ -13,3 +13,12 @@ provider "aws" {
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "student-terraform-demo-UNIQUE-ID"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "aiops-platform-sf"
+    region         = "ap-south-1"
+    key            = "aws/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
+  }
+}
