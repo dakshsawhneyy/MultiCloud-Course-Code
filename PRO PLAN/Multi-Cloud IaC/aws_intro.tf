5256c092f48970897c75be613238e487b67bc0f1
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-30 11:05:45 +0530

    Create aws_intro.tf

---
+++ PRO PLAN/Multi-Cloud IaC/aws_intro.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "student-terraform-demo-UNIQUE-ID"
+} 
