80123d795b67ed3bc67a27d6e21f1f10e1082388
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-08-31 09:45:16 +0530

    Create main.tf

---
+++ PRO PLAN/Multi-Cloud IaC/remote_backend_gcp/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = "YOUR_PROJECT_ID"
+  region  = "asia-south1"
+}
+
+resource "google_storage_bucket" "demo_bucket" {
+  name     = "student-terraform-gcp-UNIQUE-ID"
+  location = "ASIA-SOUTH1"
+  uniform_bucket_level_access = true
+}
