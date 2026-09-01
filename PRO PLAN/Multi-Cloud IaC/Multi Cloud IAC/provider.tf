9daea62a9d6f9c4c3a7557214317fcfbfebc218b
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-01 12:55:57 +0530

    Create provider.tf

---
+++ PRO PLAN/Multi-Cloud IaC/Multi Cloud IAC/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+
+  }
+}
