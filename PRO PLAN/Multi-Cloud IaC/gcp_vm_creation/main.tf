83052d583551b190b3806d48e29bee4d9ee08b61
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-01 11:33:10 +0530

    Update main.tf

--- PRO PLAN/Multi-Cloud IaC/gcp_vm_creation/main.tf
+++ PRO PLAN/Multi-Cloud IaC/gcp_vm_creation/main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "vm" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
