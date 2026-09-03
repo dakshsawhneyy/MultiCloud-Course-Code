2703927e1d13fe8b7bb4404623ae7bec4f97db71
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-03 11:16:31 +0530

    Update commands.sh

--- PRO PLAN/CICD in Multi-Cloud/gcp_application_deployment/commands.sh
+++ PRO PLAN/CICD in Multi-Cloud/gcp_application_deployment/commands.sh
@@ -1,10 +1,1 @@
-# Create Artifact Registry
-gcloud artifacts repositories create demo-repository \
---repository-format=docker \
---location=us-central1
 
-
-# Run workflow on cloud build
-gcloud builds submit \
---config cloudbuild.yaml \
-.
