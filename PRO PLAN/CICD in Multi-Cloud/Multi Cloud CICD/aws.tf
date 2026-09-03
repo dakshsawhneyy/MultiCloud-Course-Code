a3f9e22fd736072427a0af8f65e50bade0fef473
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-03 12:47:23 +0530

    Create aws.tf

---
+++ PRO PLAN/CICD in Multi-Cloud/Multi Cloud CICD/aws.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "app" {
+  name = var.app_name
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-xxxxxxxxxxxxxxxxx"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-demo-vm"
+  }
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "app_name" {
+  default = "multi-cloud-app"
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
