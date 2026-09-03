601719c887ac1937d9cc37d14099fc0f190ef15a
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-03 12:54:38 +0530

    Add SSH public key resource and update VM settings
    Added SSH public key resource and updated VM configuration to use SSH key authentication.

--- PRO PLAN/CICD in Multi-Cloud/Multi Cloud CICD/azure.tf
+++ PRO PLAN/CICD in Multi-Cloud/Multi Cloud CICD/azure.tf
@@ -26,13 +26,24 @@ resource "azurerm_container_registry" "app" {
   admin_enabled       = true
 }
 
-# 3. Simple VM (No explicit NIC, NSG, or Public IP blocks needed)
+# 3. Create the SSH Public Key Resource in Azure
+resource "azurerm_ssh_public_key" "vm_key" {
+  name                = "demo-azure-key"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  
+  # This uses a local key you already have. 
+  # If you don't have one, run 'ssh-keygen -t rsa -b 4096' in your terminal first.
+  public_key          = file("~/.ssh/id_rsa.pub")
+}
+
+# 4. Simple VM linking to the new key
 resource "azurerm_virtual_machine" "web" {
   name                  = "terraform-demo-vm"
   location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
   vm_size               = "Standard_B1s"
-  network_interface_ids = [] # Left empty for basic defaults
+  network_interface_ids = [] 
 
   storage_image_reference {
     publisher = "Canonical"
@@ -50,11 +61,17 @@ resource "azurerm_virtual_machine" "web" {
 
   os_profile {
     computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_username = "azureuser"
   }
 
+  # Link the generated Azure SSH key block here
   os_profile_linux_config {
-    disable_password_authentication = false
+    disable_password_authentication = true
+    
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = azurerm_ssh_public_key.vm_key.public_key
+    }
   }
 }
+
