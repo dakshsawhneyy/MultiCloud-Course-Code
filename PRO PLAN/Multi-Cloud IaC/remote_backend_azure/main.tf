33ae9b91647392d23730146718634a97a5c73a1d
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-04 13:31:39 +0530

    Create main.tf

---
+++ PRO PLAN/Multi-Cloud IaC/remote_backend_azure/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+    }
+  }
+}
+
+# Initialization
+provider "azurerm" {
+  features {}
+}
+
+
+
+data "azurerm_resource_group" "demo" {
+  name = "multicloud-demo-rg"
+}
+
+# Azure Storage Account
+resource "azurerm_storage_account" "demo" {
+  name = "terraformdemosa"
+  resource_group_name = azurerm_resource_group.demo.name
+  location = azurerm_resource_group.demo.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
