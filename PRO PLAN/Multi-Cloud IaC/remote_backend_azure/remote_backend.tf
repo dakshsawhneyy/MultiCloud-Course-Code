d86dc41fed795a910ab6b4df58604441f1ddb55c
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-04 13:29:12 +0530

    Create remote_backend.tf

---
+++ PRO PLAN/Multi-Cloud IaC/remote_backend_azure/remote_backend.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0" # Keeps configuration locked to stable v4 minor updates
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.rsg_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.env
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.storage.id
+  container_access_type = "private"
+}
