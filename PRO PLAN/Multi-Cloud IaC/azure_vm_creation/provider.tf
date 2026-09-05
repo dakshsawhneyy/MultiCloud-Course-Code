49c20346aca27d7490266cd641c3edcbd5bda481
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-05 10:55:24 +0530

    Create provider.tf

---
+++ PRO PLAN/Multi-Cloud IaC/azure_vm_creation/provider.tf
@@ -0,0 +1,13 @@
+Terraform and Provider Configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
