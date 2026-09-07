d1650caf0956b2c2e6c6489b3980a783b2245c56
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-09-07 10:37:08 +0530

    Update commands.sh

--- PRO PLAN/Multi-Cloud IaC/Azure ARM/commands.sh
+++ PRO PLAN/Multi-Cloud IaC/Azure ARM/commands.sh
@@ -1,9 +1,19 @@
 az group create --name arm-demo-rg --location centralindia
 
+
+
 # Validating the template
 az deployment group validate --resource-group arm-demo-rg \
 --template-file template.json --parameters storageAccountName=arm-demo-99770066
 
+
+
 # Deploy
- az deployment group create --resource-group arm-demo-rg \
- --template-file template.json --parameters storageAccountName=arm-demo-99770066
+az deployment group create --resource-group arm-demo-rg \
+--template-file template.json --parameters storageAccountName=arm-demo-99770066
+
+
+
+# Redeploy
+az deployment group create --resource-group arm-demo-rg \
+--template-file template.json --parameters storageAccountName=arm-demo-99770066
